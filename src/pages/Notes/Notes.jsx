d0754c3cc8068cdced0eacde146535d04ae1dead
sndp <sand33psm@gmail.com>
2024-11-14 17:03:16 +0530

    add loader animation on notes.jsx

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MoonIcon, SunIcon } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../slices/authSlice';
 import Note from '../../components/Note/Note';
 import api from '../../api';
 import { toggleDarkMode } from '../../slices/themeSlice';
@@ -15,7 +13,7 @@ const Notes = () => {
   const [noteAdded, setNoteAdded] = useState(false);
   const [noteDeleted, setNotedeleted] = useState(false);
   const [noteEdited, setNoteEdited] = useState(false);
-  const [loading, setLoading] = useState(true); // Add loading state
+  const [loading, setLoading] = useState(true);
 
   const toggleTheme = () => {
     dispatch(toggleDarkMode());
@@ -23,10 +21,8 @@ const Notes = () => {
 
   const deleteNote = async (id) => {
     setNotedeleted(false);
-    console.log("Id => ", id);
     try {
       const res = await api.delete(`/api/notes/${id}/`);
-      console.log(res);
       if (res.status === 204) {
         setNotedeleted(true);
         alert("Note deleted successfully.");
@@ -38,10 +34,8 @@ const Notes = () => {
 
   const editNote = async (id, { title, content }) => {
     setNoteEdited(false);
-    console.log("Iside onEdit => id ", id);
     try {
       const res = await api.put(`api/notes/${id}/`, { title, content });
-      console.log(res.data);
       setNoteEdited(true);
     } catch (error) {
       console.log(error);
@@ -56,35 +50,37 @@ const Notes = () => {
       } catch (error) {
         console.log("Failed to fetch data", error);
       } finally {
-        setLoading(false); // Set loading to false after data is fetched
+        setLoading(false);
       }
     })();
   }, [noteAdded, noteDeleted, noteEdited]);
 
   return (
     <div className={`min-h-screen transition-colors duration-100 ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
-      {/* Navbar */}
       <Navbar buttonType={"Logout"} />
 
-      <NoteCreationForm noteAdded={noteAdded} setNoteAdded={setNoteAdded} />
+      <div className="max-w-7xl mx-auto px-4 py-12">
+        {/* Note Creation Form */}
+        <NoteCreationForm noteAdded={noteAdded} setNoteAdded={setNoteAdded} />
 
-      {/* Loading Spinner */}
-      {loading && (
-        <div className="flex justify-center items-center h-screen">
-          <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-solid border-blue-600 rounded-full" role="status">
-            <span className="visually-hidden">Loading...</span>
+        {/* Loading Spinner */}
+        {loading && (
+          <div className="flex justify-center items-center py-8">
+            <div className="spinner-border animate-ping inline-block w-32 h-32 border-4 border-solid border-green-600 rounded-full   border-top-color: bg-gray-300   border-right-color: slate-500" role="status">
+              <span className="visually-hidden relative top-12 left-1 text-black">Fetching Notes</span>
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {/* Notes container */}
-      {!loading && (
-        <div className="max-w-7xl mx-auto px-4 py-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {notes.map((note) => (
-            <Note key={note.id} id={note.id} title={note.title} content={note.content} date={note.created_at} onDelete={deleteNote} onEdit={editNote} />
-          ))}
-        </div>
-      )}
+        {/* Notes container */}
+        {!loading && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <Note key={note.id} id={note.id} title={note.title} content={note.content} date={note.created_at} onDelete={deleteNote} onEdit={editNote} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
